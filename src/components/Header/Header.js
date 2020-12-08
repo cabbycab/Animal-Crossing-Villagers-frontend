51d28f7b2d866d580295e1abe585c064c1c9ca70
Danielle Koski <dkoski4@yahoo.com>
2020-12-08 12:24:29 -0800

    sets up layout with styles

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = (props) => {
+    return (
+    <header className="Header">
+        <h1>My Project 3</h1>
+        <nav>
+            <ul className="NavLink">
+                <li>Logout</li>
+                <li>Dashboard</li>
+                <li>Signup</li>
+                <li>Login</li>
+            </ul>
+        </nav>
+    </header>
+    )
+}
+
+export default Header;
